f3b07d9b87972a92d119a44cca5b1ad0edbdeefb
Anton Chkadua <anton.chkadua@prk-a.ru>
2018-12-21 14:58:10 +0300

    Add proper output for filtered WAV

--- lab5.py
+++ lab5.py
@@ -4,11 +4,21 @@ from numpy import abs, arange
 from numpy.fft import fft
 from scipy.signal import butter, lfilter
 from sys import argv
+import os.path as path
+
+
+if len(argv) < 2:
+    print("Usage: python3 lab5.py <WAV_file>")
+    exit(-1)
+
+if not path.isfile(argv[1]):
+    print(f"{argv[1]} is not a file")
+    exit(-1)
 
 data, rate = sf.read(argv[1])
 
 plt.subplots()
-plt.plot(arange(0, 131072), abs(fft(data, 131072)))
+plt.plot(arange(0, 134160), abs(fft(data)))
 plt.show()
 
 b, a = butter(10, 3000 / (rate / 2), btype="lowpass", output="ba")
@@ -18,4 +28,4 @@ plt.subplots()
 plt.plot(arange(0, 131072), abs(fft(filtered, 131072)))
 plt.show()
 
-sf.write(argv[1].split(".")[0] + "_filt_sample.wav", filtered, rate)
+sf.write(f"{path.basename(argv[1]).split('.')[0][6:]}_filt_sample.wav", filtered, rate)
